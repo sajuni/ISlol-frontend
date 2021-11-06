81e381bc8544b580b1cfd423599abc0d7a1738bc
SeungHyo <sajuni11@naver.com>
2021-11-06 16:41:47 +0900

    구조잡는중

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,31 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-const LoginOn = () => import('@/views/session/LoginOn')
-const SignUp = () => import('@/views/session/SignUp')
-const Main = () => import('@/views/Main')
+const LoginOn = () =>
+    import ('@/views/session/LoginOn')
+const SignUp = () =>
+    import ('@/views/session/SignUp')
+const Main = () =>
+    import ('@/views/Main')
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "LoginOn",
-      component: LoginOn,
-    },
-    {
-      path: "/sginup",
-      name: "SignUp",
-      component: SignUp
-    },
-    {
-      path: "/main",
-      name: "Main",
-      component: Main
-    }
-  ]
+    mode: "history",
+    routes: [{
+            path: "/",
+            name: "LoginOn",
+            component: LoginOn,
+        },
+        {
+            path: "/sginup",
+            name: "SignUp",
+            component: SignUp
+        },
+        {
+            path: "/main",
+            name: "Main",
+            component: Main
+        }
+    ]
 });
