ebc15e2dca5af043fa4227cdd1b8979a01d31ae0
Seung Hyo <sajuni11@naver.com>
2022-01-28 18:29:40 +0900

    경로 이상 현상

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,7 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
         overlay: false,
+        historyApiFallback: true,
         proxy: {
             '/api': {
                 target: 'http://localhost:8180',
