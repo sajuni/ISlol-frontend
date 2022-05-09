c6ae1e6be1d5886a0ebf4fd137fe4db88668cddf
DESKTOP-JMCULO8\sajun <sajuni11@naver.com>
2022-05-09 11:23:54 +0900

    메인 초기화

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-import authApi from './auth';
-import noticeApi from './notice';
-import mediaApi from './media';
-import kakaoApi from './kakaoapi';
-
-export { authApi, noticeApi, mediaApi, kakaoApi };
--- src/plugins/global-components.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import GlobalTitleArea from '@/views/layouts/Title.vue';
-import AdminLayout from '@/views/layouts/AdminLayout.vue';
-
-Vue.component('t-area', GlobalTitleArea);
-Vue.component('a-area', AdminLayout);
