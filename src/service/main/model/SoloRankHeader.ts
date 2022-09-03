493e113c0cc5ccb20ed9393d0c9f36b5e5298ffb
SeungHyo <sajuni11@naver.com>
2022-09-03 18:08:24 +0900

    전적검색 크롤링 작업 중

---
+++ src/service/main/model/SoloRankHeader.ts
@@ -0,0 +1,8 @@
+export interface SoloRankHeader {
+  text: string;
+  img: string;
+  class: string;
+  score: string;
+  history: string;
+  rate: string;
+}
--- src/service/main/moedules/main.ts
+++ src/service/main/moedules/main.ts
@@ -1,17 +1,42 @@
 import pinia from '@/store';
 import { defineStore } from 'pinia';
 import { search } from '../api/main';
+import { SoloRankHeader } from '../model/SoloRankHeader';
 
 const cheerio = require('cheerio');
 
+interface HistorySearchState {
+  soloRankHeader: SoloRankHeader;
+}
+
 const mainStore = defineStore({
   id: 'main',
+  state: (): HistorySearchState => ({
+    soloRankHeader: {
+      text: '',
+      class: '',
+      history: '',
+      img: '',
+      rate: '',
+      score: '',
+    },
+  }),
+
   actions: {
     async search(searchVal: string): Promise<any> {
       try {
         const data = await search(searchVal);
         const $ = cheerio.load(data.data);
-        console.log($());
+        debugger;
+        const rankHeader = $('.e1x14w4w1');
+        rankHeader.each((i: number, el: string) => {
+          if (i == 0) {
+            // 솔로랭크 헤더
+            this.soloRankHeader.text = $(el).find('.header').text();
+            console.log(this.soloRankHeader.text);
+            this.soloRankHeader.img = $(el).find('.content img').attr('href');
+          }
+        });
         return data;
       } catch (err) {
         return Promise.reject(err);
